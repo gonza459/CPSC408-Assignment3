7e59a73728274d1807f67737ba156e0eaad1ed24
gonza459 <gonza459@mail.chapman.edu>
2019-04-17 09:37:08 -0700

    csv fil

---
+++ DBconfig.py
@@ -0,0 +1,30 @@
+from faker import Faker
+import csv
+
+faker = Faker()
+
+def writeToCSV():
+    with open(fileName, 'w') as csvfile:
+        fieldnames = ['first_name', 'last_name', 'address', 'email', 'creditCardNum', 'cardExpiration']
+
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+
+        writer.writeheader()
+        for i in range(int(numTuples)):
+            writer.writerow(
+                {
+                    'first_name': faker.first_name(),
+                    'last_name': faker.last_name(),
+                    'address': faker.address(),
+                    'email': faker.email(),
+                    'creditCardNum': faker.credit_card_number(),
+                    'cardExpiration': faker.credit_card_expire()
+                }
+            )
+
+if __name__ == '__main__':
+    fileName = input('Enter the name of the file you want to create: ')
+    fileName = fileName + '.csv'
+    numTuples = input('Enter the number of tuples you would like: ')
+    writeToCSV()
+
